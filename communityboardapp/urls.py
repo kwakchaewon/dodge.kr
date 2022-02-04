f62bd77b49ff8b1b335c40ddbd6e8170a8fb2347
kwakchaewon <74225015+kwakchaewon@users.noreply.github.com>
2022-02-05 00:03:17 +0900

    Update signup.html & signup.css
    make basic structure of signup.html

--- communityboardapp/urls.py
+++ communityboardapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from communityboardapp.views import goToMain, goLogin
+from communityboardapp.views import goToMain, goLogin, goSignUp
 
 urlpatterns = [
     # path('cbvList', BoardListClassView.as_view(), name='cbv'),
     path('', goToMain,),
     path('login', goLogin,),
+    path('signup', goSignUp,),
 ]
