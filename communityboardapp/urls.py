ba92dee0c3acddcd32da76222bb3615fa8abfb25
kwakchaewon <74225015+kwakchaewon@users.noreply.github.com>
2022-02-10 22:07:35 +0900

    Update logout

--- communityboardapp/urls.py
+++ communityboardapp/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('signupCompleted', signupCompleted, ),
     path('userIdCheck', userIdCheck),
     path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login.html'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
