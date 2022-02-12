e8248b3ba46ced28e0b8606c8e7ed2093c2b6d44
kwakchaewon <74225015+kwakchaewon@users.noreply.github.com>
2022-02-13 06:54:37 +0900

    Update views.py, html in communityboardapp
    build basic structure of goCommunity view & template

--- communityboardapp/views.py
+++ communityboardapp/views.py
@@ -3,10 +3,13 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from testapp.models import Boards
+import math
+
 
 # 메인 페이지 이동
 def goToMain(request):
     return render(request, 'main.html')
+
 
 # 로그인 페이지 이동
 # def goLogin(request):
@@ -15,15 +18,65 @@ def goToMain(request):
 def goSignUp(request):
     return render(request, 'signup.html')
 
+
 # 커뮤니티 이동
 def goCommunity(request):
+
+    # page : 현재 선택한 페이지
+    page = 3
+
+    # boardsCount : 전체 게시글 개수
     boards = Boards.objects.all()
-    print(boards)
-    return render(request, 'communityboard.html')
+    boardsCount = boards.count()
+
+    # initialBoard : 표기되는 첫 게시글 No / finalBoard : 표기되는 마지막 게시글 No
+    initialBoard = boardsCount - 10 * (page)
+    finalBoard = boardsCount - 10 * (page) + 10
+    # page = math.ceil(boardsCount / 10)
+
+
+    # initialPage : 표기되는 첫 페이지 / finalPage : 표기되는 마지막 페이지
+    initialPage = math.trunc(page/10)*10+1
+
+    if boardsCount > 10 * (initialPage + 9):
+        finalPage = initialPage + 9
+    else:
+        finalPage = math.ceil(boardsCount / 10)
+
+    pageList = []
+    for i in range(initialPage, finalPage+1):
+        pageList.append(i)
+
+    print('iinitialPage: ', initialPage, ', finalPage: ', finalPage)
+
+    # select * from Boards where id>initialBoard and id<=finalBoard order by id Desc
+    boards = Boards.objects.filter(id__gt=initialBoard,
+                                       id__lte=finalBoard).order_by('-id')
+
+    return render(request, 'communityboard.html', {'boards': boards, 'pageList': pageList})
+
+
+
+    # if page == '':
+    #     page = 750
+    #
+    #     # 71개라 치면 1페이지일때 71부터 62까지
+    #     # 2페이지면 61부터 52까지
+    #     boards = Boards.objects.filter(id__gt=boardsCount - 10 * (page),
+    #                                    id__lte=boardsCount - 10 * (page) + 9).order_by('-id')
+    #     return render(request, 'communityboard.html', {'boards': boards})
+    #
+    #
+    # else:
+    #     page = 1
+    #     boards = Boards.objects.filter(id__gt=boardsCount - 10 * (page),
+    #                                    id__lte=boardsCount - 10 * (page) + 9).order_by('-id')
+    #     boards = Boards.objects.filter(id__gt=boardsCount - 10, id__lte=boardsCount).order_by('-id')
+    #     return render(request, 'communityboard.html', {'boards': boards})
+
 
 # 회원가입
 def signupCompleted(request):
-
     if request.method == "POST":
         username = request.POST['container__id']
         password = request.POST['container__pw']
@@ -48,21 +101,15 @@ def signupCompleted(request):
         except:
             redirect('error')
 
-
-
-
         # 사용자인증 및 로그인
         # loginUser = authenticate(username=username, password=password)
         # login(request, loginUser)
-
-
 
     return redirect('error')
 
 
 # 아이디 중복 확인
 def userIdCheck(request):
-
     if request.method == "POST":
         username = request.POST.get('username', False)
 
@@ -82,14 +129,13 @@ def userIdCheck(request):
 
     return HttpResponse(msg)
 
+
 def loginCompleted(request):
     return render(request, 'main.html')
 
 
 def goErrorPage(request):
     return render(request, 'errorpage.html')
-
-
 
 # def goRegistUser(request):
 #  # return redirect('accounts:login')
