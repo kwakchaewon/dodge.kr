9997c819ff48af276e6d9b07810c009825cc42f1
kwakchaewon <74225015+kwakchaewon@users.noreply.github.com>
2022-03-16 00:28:44 +0900

    Update else

--- djangowebapp/urls.py
+++ djangowebapp/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('testapp/', include('testapp.urls')),
     path('', include('communityboardapp.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
