01d2f14ae6432ece67ee9027d0e003eac72fca82
kwakchaewon <74225015+kwakchaewon@users.noreply.github.com>
2022-03-17 01:12:28 +0900

    Update urls.py

--- communityboardapp/urls.py
+++ communityboardapp/urls.py
@@ -6,12 +6,12 @@ urlpatterns = [
     # path('cbvList', BoardListClassView.as_view(), name='cbv'),
     # path('url', view, name=''),
     path('', goToMain, name='main'),
-    path('signup', goSignUp,),
+    path('signup', goSignUp, ),
     path('community', goCommunity, name='community'),
     path('community/write', writePost, ),
     path('boardwriteCompleted', boardwriteCompleted, name='boardwriteCompleted'),
     path('signupCompleted', signupCompleted, ),
-    path('userIdCheck', userIdCheck,),
+    path('userIdCheck', userIdCheck, ),
     path('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('error', goErrorPage),
@@ -19,5 +19,5 @@ urlpatterns = [
     path('insertComment', insertComment, name='insertComment'),
     path('deleteBoard/<int:id>/', deleteBoard, name='deleteBoard'),
     path('editBoard/<int:id>/', editBoard, name='editBoard'),
-
+    path('editBoardCompleted', editBoardCompleted, name='editBoardCompleted'),
 ]
