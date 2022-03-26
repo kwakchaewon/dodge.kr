675f8389e609988bb5cf31b124ce57c0b4d4558f
kwakchaewon <74225015+kwakchaewon@users.noreply.github.com>
2022-03-26 10:06:41 +0900

    Update else

--- static/js/communityboard.js
+++ static/js/communityboard.js
@@ -1,8 +1,6 @@
 function searchBoard(){
     var searchType = document.getElementById('search__select').value;
     var searchWord = document.getElementById('searchword__input__value').value;
-    alert(searchType);
-    alert(searchWord);
     location.href= "?searchType=" + searchType +"&searchWord="+searchWord;
 }
 
